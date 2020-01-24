f7a9581d62ba27a70c7df9191c88f924b74c28e3
uma-1 <mndruma@gmail.com>
2020-01-24 16:31:25 +0545

    details

---
+++ app/src/main/java/com/example/pharmacy_health_care/MobileDetailActivity.java
@@ -0,0 +1,86 @@
+package com.example.pharmacy_health_care;
+
+import android.content.Intent;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.GridLayoutManager;
+//import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import API.API;
+import adapter.ItemsAdapter;
+import model.Items;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import reusable.Reusable;
+
+public class MobileDetailActivity extends AppCompatActivity {
+    private Button buttonback;
+    private RecyclerView rvItems;
+    List<Items> itemList = new ArrayList<>();
+    String selectedName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_detail);
+
+        rvItems= findViewById(R.id.mobileDetailRv);
+        buttonback = findViewById(R.id.btnbacks);
+        buttonback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MobileDetailActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Bundle bundle = getIntent().getExtras();
+        selectedName = bundle.getString("selectedName");
+
+        API Api = Reusable.getInstance().create(API.class);
+
+        Call<List<Items>> listCall = Api.getAllItems();
+
+        listCall.enqueue(new Callback<List<Items>>() {
+            @Override
+            public void onResponse(Call<List<Items>> call, Response<List<Items>> response) {
+
+                generateList(response.body());
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Items>> call, Throwable t) {
+                Toast.makeText(MobileDetailActivity.this, "Error : "+t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private void generateList( List<Items> body) {
+        List<Items> itemList = body;
+        List<Items> itemsArrayList = new ArrayList<>();
+
+        for (Items item: itemList){
+            String image= item.getImageName();
+
+            if ( item.getName().equals(selectedName) ){
+                itemsArrayList.add(new Items(item.getName(), item.getModule(), item.getImageName(), item.getSize(), item.getRam(),item.getRom(),item.getOs(),item.getFcamera(),item.getBcamera(),item.getPrice()));
+            }
+
+        }
+        ItemsAdapter itemAdapter = new ItemsAdapter(this, itemsArrayList);
+        rvItems.setAdapter(itemAdapter);
+        rvItems.setLayoutManager(new GridLayoutManager(this, 1));
+    }
+}
