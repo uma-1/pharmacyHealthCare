1ba44b581a76c14cfb6dabedad5f3dead950e912
uma-1 <49069895+uma-1@users.noreply.github.com>
2020-02-06 22:32:25 +0545

    url

---
+++ app/src/main/java/com/example/pharmacy_health_care/url/url.java
@@ -0,0 +1,21 @@
+package com.example.pharmacy_health_care.url;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class url {
+    public static final String base_url = "http://10.0.2.2:3000/";
+    public static final String base_url_image = "http://10.0.2.2:3000/upload/";
+    // public static final String base_url = "http://172.100.100.5:3000/";
+    public static String token = "Bearer ";
+    public static String imagePath = base_url + "uploads/" ;
+
+    public static Retrofit getInstance() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(base_url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+}
+
