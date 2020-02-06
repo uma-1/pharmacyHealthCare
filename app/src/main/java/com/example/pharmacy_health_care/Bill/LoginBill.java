7a7e9bbfbc86123cf8901371fb08e864a3ed1dbd
uma-1 <49069895+uma-1@users.noreply.github.com>
2020-02-06 22:32:05 +0545

    updated

--- app/src/main/java/com/example/pharmacy_health_care/Bill/LoginBill.java
+++ app/src/main/java/com/example/pharmacy_health_care/Bill/LoginBill.java
@@ -1,35 +1,42 @@
 package com.example.pharmacy_health_care.Bill;
 
-public class LoginBill {
+import com.example.pharmacy_health_care.url.url;
 
-//    private String username;
-//    private String password;
-//    boolean isSuccess = false;
-//
-//    public LoginBill(String username, String password) {
-//        this.username = username;
-//        this.password = password;
-//    }
-//
-//    public boolean checkUser() {
-//
-//        Users user = new Users(username, password);
-//        API api = Reusable.getInstance().create(API.class);
-//        Call<LoginSignupResponse> usercall = api.loginUser(user);
-//
-//        try
-//        {
-//            Response<LoginSignupResponse>loginSignupResponseResponse = usercall.execute();
-//            if (loginSignupResponseResponse.body().getSuccess()) {
-//                isSuccess = true;
-//
-//            }
-//        }
-//        catch (IOException e){
-//            e.printStackTrace();
-//        }
-//        return isSuccess;
-//
-//
-//    }
+import java.io.IOException;
+
+import API.UserApi;
+import model.LoginSignupResponse;
+import model.Users;
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class LoginBill {
+    boolean isSuccess = false;
+
+    public boolean checkUser(String email, String password){
+
+        UserApi userApi = url.getInstance().create(UserApi.class);
+        Users user = new Users(email,password);
+        Call<LoginSignupResponse> userCall =userApi.checkUser(user);
+
+        try {
+            Response<LoginSignupResponse> loginResponse = userCall.execute();
+            if (loginResponse.isSuccessful()) {
+                if(loginResponse.body().getStatus()) {
+                    isSuccess = true;
+                }
+//                else if(loginResponse.body().getStatus().equals("false") ){
+//                    isSuccess = false;
+//                }
+            }
+            else{
+                isSuccess = false;
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        return isSuccess;
+    }
 }
