f53b0d0742c107a4ca26badedc6ad96edcfcedd8
uma-1 <49069895+uma-1@users.noreply.github.com>
2020-02-15 14:52:34 +0545

    updated

---
+++ app/src/test/java/com/example/pharmacy_health_care/LoginTest.java
@@ -0,0 +1,14 @@
+package com.example.pharmacy_health_care;
+
+public class LoginTest {
+
+
+//    @Test
+//    public  void testLogin()
+//    {
+//
+//        LoginBill loginBill = new LoginBill("bijay", "bijay");
+//        boolean result = loginBill.checkUser();
+//        assertEquals(true, result);
+//    }
+}
